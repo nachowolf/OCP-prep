15f96a3ba94488a13983801ccb05783cd6ee10a6
nachowolf <nathrijacobs@gmail.com>
2019-09-20 15:59:25 +0200

    initial commit

---
+++ src/main/java/i_o_and_n_o/tobias_motor_repairs/service_app/tracker/CarBrand.java
@@ -0,0 +1,8 @@
+package i_o_and_n_o.tobias_motor_repairs.service_app.tracker;
+
+public enum CarBrand {
+    Ford,
+    Toyota,
+    Nissan,
+    Other
+}
---
+++ src/main/java/i_o_and_n_o/tobias_motor_repairs/service_app/tracker/Main.java
@@ -0,0 +1,40 @@
+package i_o_and_n_o.tobias_motor_repairs.service_app.tracker;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void sortBrand(){}
+
+
+
+    public static void main(String[] args) {
+        try {
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+            Path path = Paths.get("src/main/java/i_o_and_n_o/tobias_motor_repairs/service_app/service");
+            path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+            watchService.poll(5, TimeUnit.SECONDS);
+
+//            File file;
+
+            WatchKey watchKey;
+            while ((watchKey = watchService.take()) != null){
+                for(WatchEvent<?> event : watchKey.pollEvents()){
+
+                }
+            }
+
+
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
